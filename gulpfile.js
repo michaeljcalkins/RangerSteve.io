87259b1cfad86a5e215f27242ed73427f5be49af
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-03-08 00:17:31 -0800

    fixes to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var handleError = function (err) {
 };
 
 gulp.task('sass', function() {
-    return gulp.src(SRC + 'scss/app.scss')
+    return gulp.src('assets/scss/app.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             sourcemap: true,
