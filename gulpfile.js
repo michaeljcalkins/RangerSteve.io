66f2d8465dce953ddd3c6fe0a398acf83d7dc4fe
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-04-26 08:15:17 -0700

    built for prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
-var elixir = require('laravel-elixir')
+var gulp = require('gulp'),
+    elixir = require('laravel-elixir'),
+    jsObfuscator = require('gulp-js-obfuscator'),
+    streamify = require('gulp-streamify')
 
 require('laravel-elixir-vueify')
 
 elixir.config.assetsPath = 'assets'
 
+elixir.extend('obfuscate', function() {
+    new elixir.Task('obfuscate', function() {
+        return gulp.src('public/javascripts/app.js')
+            .pipe(streamify(jsObfuscator()))
+            .pipe(gulp.dest('public/javascripts'))
+    })
+})
+
 elixir(function(mix) {
+    if (! elixir.config.production) {
+        return mix
+            .sass('assets/sass/app.scss', 'public/stylesheets/app.css')
+            .browserify('assets/js/app.js', 'public/javascripts/app.js')
+    }
+
     mix
-        .sass('./assets/sass/app.scss', './public/stylesheets/app.css')
-        .browserify('./assets/js/app.js', './public/javascripts/app.js')
+        .sass('assets/sass/app.scss', 'public/stylesheets/app.css')
+        .browserify('assets/js/app.js', 'public/javascripts/app.js')
+        .obfuscate()
 })
