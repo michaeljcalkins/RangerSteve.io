14de22ed7840dfa965d48283c8c9b49cbfe346bd
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-04-26 08:43:47 -0700

    added a twist to the player so they look more natural when running

--- assets/js/lib/PlayerMovementHandler.js
+++ assets/js/lib/PlayerMovementHandler.js
@@ -9,13 +9,25 @@ export default function PlayerMovementHandler() {
         this.player.animations.play('left')
 
         // Left facing head needs to be set only once
-        playerFaceLeft.call(this)
+        if (this.game.input.worldX > this.player.x) {
+            playerFaceRight.call(this)
+        }
+
+        if (this.game.input.worldX < this.player.x) {
+            playerFaceLeft.call(this)
+        }
     } else if (rightInputIsActive.call(this)) {
         // If the RIGHT key is down, set the player velocity to move right
         this.player.body.acceleration.x = GameConsts.ACCELERATION
         this.player.animations.play('right')
 
-        playerFaceRight.call(this)
+        if (this.game.input.worldX > this.player.x) {
+            playerFaceRight.call(this)
+        }
+
+        if (this.game.input.worldX < this.player.x) {
+            playerFaceLeft.call(this)
+        }
     } else {
         // Stand still
         this.player.body.acceleration.x = 0
