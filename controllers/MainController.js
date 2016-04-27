46bf84aaf92fe3f99d309ab8d5e3a42ada203876
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-04-26 17:27:02 -0700

    shrunk images

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,8 +1,8 @@
 module.exports = {
-    home: function(req, res, next) {
+    home: function(req, res) {
         res.render('home', { title: 'Home | Ranger Steve: Buffalo Invasion' });
     },
-    game:  function(req, res, next) {
+    game:  function(req, res) {
         res.render('game', { title: 'Game | Ranger Steve: Buffalo Invasion' });
     }
 }
