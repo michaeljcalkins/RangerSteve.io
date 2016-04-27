85b1c41f2f40106f06c10496f20a3e2225e7ea5f
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-04-26 21:47:08 -0700

    added cache control and html for favicon

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    maxAge: 86400000 // 1d
+}));
 
 app.use('/', routes)
 
