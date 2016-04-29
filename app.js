fe815a65f821eaf0132e15af4e4d4b8817023f31
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-04-29 15:16:37 -0700

    inital conversion to nunjucks

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression')
+var nunjucks = require('nunjucks')
 require('http').Server(app)
 var SocketHandler = require('./lib/SocketHandler')
 
@@ -23,7 +24,12 @@ SocketHandler(io)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'nunjucks');
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(compression())
