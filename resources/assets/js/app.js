dbd147ff1b1e84d87f03df308a0cc12c3e6aa4f9
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-05-07 14:48:26 -0700

    removed toastr

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,3 @@
-toastr.options = {
-    closeButton: false,
-    debug: false,
-    newestOnTop: false,
-    progressBar: false,
-    positionClass: 'toast-top-center',
-    preventDuplicates: false,
-    onclick: null,
-    showDuration: 300,
-    hideDuration: 1000,
-    timeOut: 3000,
-    extendedTimeOut: 1000,
-    showEasing: 'swing',
-    hideEasing: 'linear',
-    showMethod: 'fadeIn',
-    hideMethod: 'fadeOut'
-}
-
 function check(object, propTypes, _throw) {
     let propName;
 
