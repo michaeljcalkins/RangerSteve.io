451f56ce21635186a0c35c67ef5920f2888c5208
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-06-02 08:41:14 -0700

    converted the bullet sprites to an init function

--- resources/assets/js/lib/InitHandler/index.js
+++ resources/assets/js/lib/InitHandler/index.js
@@ -2,9 +2,11 @@ import InitEvents from './InitEvents'
 import InitHurtBorder from './InitHurtBorder'
 import InitMapAndPlayer from './InitMapAndPlayer'
 import InitWindowEvents from './InitWindowEvents'
+import InitBullets from './InitBullets'
 
 export default function() {
     InitMapAndPlayer.call(this)
+    InitBullets.call(this)
     InitEvents.call(this)
     InitHurtBorder.call(this)
     InitWindowEvents.call(this)
