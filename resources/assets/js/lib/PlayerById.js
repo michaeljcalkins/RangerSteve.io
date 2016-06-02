4aa82bd93fe01a9e1b57c388c6479c56abae158a
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-06-02 08:33:00 -0700

    added sound effects in the distance

--- resources/assets/js/lib/PlayerById.js
+++ resources/assets/js/lib/PlayerById.js
@@ -7,6 +7,8 @@ const propTypes = {
 export default function PlayerById(id) {
     check({ id }, propTypes)
 
+    if (! this.enemies) return
+
     for (let i = 0; i < this.enemies.children.length; i++) {
         if (this.enemies.children[i].id === id) {
             return this.enemies.children[i]
