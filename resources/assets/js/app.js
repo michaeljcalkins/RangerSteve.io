0c3ffdb459d8c4bd66754088edfa7664198af842
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-06-11 19:03:29 -0700

    added the new math random function

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,17 @@ import Check from './lib/Check'
 import ui from './ui'
 import game from './game'
 
+Math.random = (function(rand) {
+    var salt = 0
+    document.addEventListener('mousemove', function(event) {
+        salt = event.pageX * event.pageY * window.innerWidth * window.innerHeight
+    })
+
+    return function() {
+        return (rand() + (1 / (1 + salt))) % 1;
+    }
+})(Math.random)
+
 if (storage.get('banned') === true) {
     window.location = '/'
 }
