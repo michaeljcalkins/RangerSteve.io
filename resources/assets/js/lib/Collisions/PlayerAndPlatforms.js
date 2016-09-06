ddb626ce398fcd40709fa621a3991caae44b625f
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-09-06 14:49:31 -0700

    removed this.platform references

--- resources/assets/js/lib/Collisions/PlayerAndPlatforms.js
+++ resources/assets/js/lib/Collisions/PlayerAndPlatforms.js
@@ -1,6 +1,4 @@
 export default function() {
-    this.physics.arcade.collide(this.player, this.platforms)
-
     // Collide the player against the collision layer
     this.physics.arcade.collide(this.player, this.ground)
 }
