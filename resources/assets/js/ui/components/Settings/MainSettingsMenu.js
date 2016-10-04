cda58aafc1ab6617041b6a56d8234fb6a5ec6123
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-10-03 22:22:30 -0700

    removed the soundtrack and added back the leaderboard

--- resources/assets/js/ui/components/Settings/MainSettingsMenu.js
+++ resources/assets/js/ui/components/Settings/MainSettingsMenu.js
@@ -178,19 +178,6 @@ export default class MainSettingsMenu extends React.Component {
                             />
                         </div>
                     </div>
-                    <div className="col-sm-6">
-                        <div className="form-group">
-                            <label>Music Volume</label>
-                            <input
-                                defaultValue={ this.state.musicVolume }
-                                max=".3"
-                                min="0"
-                                onChange={ this.handleMusicVolumeChange }
-                                step=".01"
-                                type="range"
-                            />
-                        </div>
-                    </div>
                 </div>
             </div>
         )
