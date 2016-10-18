48e3723217e292b3ca9dc7222e688a47544ae449
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-10-18 08:13:29 -0700

    Remove DarkForest from the map rotation

--- resources/assets/js/lib/Maps/index.js
+++ resources/assets/js/lib/Maps/index.js
@@ -1,13 +1,11 @@
 import * as HighRuleJungle from './HighRuleJungle'
 import * as PunkFallout from './PunkFallout'
-import * as DarkForest from './DarkForest'
 import * as PunkCity from './PunkCity'
 import * as PunkLoop from './PunkLoop'
 
 export default {
     HighRuleJungle,
     PunkFallout,
-    DarkForest,
     PunkCity,
     PunkLoop
 }
