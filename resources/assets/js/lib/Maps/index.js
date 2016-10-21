4acd9cec17730f5e44882a973d74a8f75f1a28e6
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-10-21 09:10:32 -0700

    Add death cycle map to the map rotation

--- resources/assets/js/lib/Maps/index.js
+++ resources/assets/js/lib/Maps/index.js
@@ -2,10 +2,12 @@ import * as HighRuleJungle from './HighRuleJungle'
 import * as PunkFallout from './PunkFallout'
 import * as PunkCity from './PunkCity'
 import * as PunkLoop from './PunkLoop'
+import * as DeathCycle from './DeathCycle'
 
 export default {
     HighRuleJungle,
     PunkFallout,
     PunkCity,
-    PunkLoop
+    PunkLoop,
+    DeathCycle
 }
