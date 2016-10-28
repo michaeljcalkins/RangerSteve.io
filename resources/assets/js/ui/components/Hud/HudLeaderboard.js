16b16d97da7a778ea164c0c1c25d9643b5e29dc0
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-10-28 12:57:25 -0700

    Update leaderboard style

--- resources/assets/js/ui/components/Hud/HudLeaderboard.js
+++ resources/assets/js/ui/components/Hud/HudLeaderboard.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import _ from 'lodash'
 
 export default function HudLeaderboard({
-    players
+    players,
 }) {
     function renderPlayers() {
         if (! players) return null
@@ -22,11 +22,9 @@ export default function HudLeaderboard({
 
                 return (
                     <tr key={ key }>
-                        <td>
-                            <strong>{ killingSpreeCount }</strong>
-                            { playerNickname }
-                        </td>
-                        <td>{ player.meta.score }</td>
+                        <td title="Player's nickname" style={ { width: '120px', overflow: 'hidden' } }>{ playerNickname }</td>
+                        <td title="Player's current killing spree" style={ { width: '20px' } }><strong>{ killingSpreeCount }</strong></td>
+                        <td title="Player's current score" style={ { width: '20px' } }>{ player.meta.score }</td>
                     </tr>
                 )
             })
