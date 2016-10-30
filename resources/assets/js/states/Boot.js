d24e4b7a62b955c9d1dde42e9f691d7c509f87a9
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-10-30 13:22:38 -0700

    Fix loading screen

--- resources/assets/js/states/Boot.js
+++ resources/assets/js/states/Boot.js
@@ -12,7 +12,24 @@ function Boot(game) {
 
 Boot.prototype = {
 
+    preload: function() {
+        this.game.load.image('ranger-steve', '/images/ranger-steve.png')
+    },
+
     create: function() {
+        // Loading screen
+        this.game.stage.backgroundColor = '#2B2825'
+        RS.rangerSteveSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'ranger-steve')
+        RS.rangerSteveSprite.anchor.setTo(0.5)
+
+        const style = {
+            font: "35px Bangers",
+            fill: "#fff",
+        }
+        let text = this.game.add.text(RS.rangerSteveSprite.x - 20, RS.rangerSteveSprite.y + 110, 'Loading...', style)
+        text.anchor.set(0.5)
+        text.smoothed = true
+
         window.socket = io.connect()
         setEventHandlers.call(this)
 
