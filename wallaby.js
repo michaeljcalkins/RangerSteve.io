bcc8ce491f3852c5313f42d0969f05f1513c10d0
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-11-01 09:03:42 -0700

    Add client js to wallaby

--- wallaby.js
+++ wallaby.js
@@ -2,10 +2,12 @@ module.exports = function (wallaby) {
     return {
         files: [
             'app/**/!(*.test).js',
+            'resources/assets/**/!(*.test).js',
         ],
 
         tests: [
             'app/**/*.test.js',
+            'resources/assets/**/*.test.js',
         ],
 
         compilers: {
