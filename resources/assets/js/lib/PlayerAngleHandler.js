6d96a62d806ba3fb09b3e59b5d05856fb2cbf4b7
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-11-03 10:01:03 -0700

    Fix the alignment of left/right arms

--- resources/assets/js/lib/PlayerAngleHandler.js
+++ resources/assets/js/lib/PlayerAngleHandler.js
@@ -8,53 +8,53 @@ export default function PlayerAngleHandler() {
     if (state.player.facing === 'right') {
         // User is aiming up and to the right
         if (angleInDegrees <= 81 && angleInDegrees >= 71) {
-            leftAngle -= 10
-            rightAngle -= 1
+            leftAngle -= -10
+            rightAngle -= 3
         } else if (angleInDegrees < 71 && angleInDegrees >= 61) {
-            leftAngle -= 20
-            rightAngle -= 2
+            leftAngle -= 0
+            rightAngle -= 4
         } else if (angleInDegrees < 61 && angleInDegrees >= 51) {
-            leftAngle -= 30
-            rightAngle -= 2
+            leftAngle -= 10
+            rightAngle -= 4
         } else if (angleInDegrees < 51 && angleInDegrees >= 41) {
-            leftAngle -= 40
-            rightAngle -= 2
+            leftAngle -= 20
+            rightAngle -= 4
         } else if (angleInDegrees < 41 && angleInDegrees >= 31) {
-            leftAngle -= 50
-            rightAngle -= 2
+            leftAngle -= 20
+            rightAngle -= 4
         } else if (angleInDegrees < 31 && angleInDegrees >= 21) {
-            leftAngle -= 60
-            rightAngle -= 3
+            leftAngle -= 20
+            rightAngle -= 5
         } else if (angleInDegrees < 21 && angleInDegrees >= 11) {
-            leftAngle -= 70
-            rightAngle -= 3
+            leftAngle -= 20
+            rightAngle -= 5
         } else if (angleInDegrees < 11 && angleInDegrees >= 0) {
-            leftAngle -= 80
-            rightAngle -= 3
+            leftAngle -= 20
+            rightAngle -= 5
         }
 
         // User is aiming down and to the right
-        if (angleInDegrees >= 82 && angleInDegrees <= 91 ) {
-            leftAngle += 5
-            rightAngle -= 1
+        if (angleInDegrees >= 81 && angleInDegrees <= 91 ) {
+            leftAngle -= -5
+            rightAngle -= 7
         } else if (angleInDegrees > 91 && angleInDegrees <= 99) {
             leftAngle += 15
-            rightAngle -= 1
+            rightAngle -= 7
         } else if (angleInDegrees > 99 && angleInDegrees <= 109) {
             leftAngle += 35
-            rightAngle -= 1
+            rightAngle -= 7
         } else if (angleInDegrees > 109 && angleInDegrees <= 119) {
             leftAngle += 45
-            rightAngle -= 3
+            rightAngle -= 7
         } else if (angleInDegrees > 119 && angleInDegrees <= 129) {
             leftAngle += 55
-            rightAngle -= 4
+            rightAngle -= 7
         } else if (angleInDegrees > 129 && angleInDegrees <= 139) {
             leftAngle += 60
-            rightAngle -= 5
+            rightAngle -= 7
         } else if (angleInDegrees > 139 && angleInDegrees <= 149) {
             leftAngle += 70
-            rightAngle -= 6
+            rightAngle -= 7
         } else if (angleInDegrees > 149 && angleInDegrees <= 159) {
             leftAngle += 80
             rightAngle -= 7
@@ -73,22 +73,22 @@ export default function PlayerAngleHandler() {
     if (state.player.facing === 'left') {
         // User is aiming up and to the left
         if (angleInDegrees >= -91 && angleInDegrees <= -81) {
-            leftAngle += -10
+            leftAngle += 20
             rightAngle -= 9
         } else if (angleInDegrees > -81 && angleInDegrees <= -71) {
-            leftAngle += 0
+            leftAngle += 20
             rightAngle -= 8
         } else if (angleInDegrees > -71 && angleInDegrees <= -61) {
-            leftAngle += 10
+            leftAngle += 30
             rightAngle -= 8
         } else if (angleInDegrees > -61 && angleInDegrees <= -51) {
-            leftAngle += 25
+            leftAngle += 40
             rightAngle -= 6
         } else if (angleInDegrees > -51 && angleInDegrees <= -41) {
-            leftAngle += 30
+            leftAngle += 40
             rightAngle -= 6
         } else if (angleInDegrees > -41 && angleInDegrees <= -31) {
-            leftAngle += 40
+            leftAngle += 50
             rightAngle -= 5
         } else if (angleInDegrees > -31 && angleInDegrees <= -21) {
             leftAngle += 50
@@ -103,31 +103,31 @@ export default function PlayerAngleHandler() {
 
         // User is aiming down and to the left
         if (angleInDegrees <= 270 && angleInDegrees >= 260) {
-            leftAngle -= 20
+            leftAngle += 10
             rightAngle -= 9
         } else if (angleInDegrees < 260 && angleInDegrees >= 250) {
-            leftAngle -= 30
+            leftAngle += 10
             rightAngle -= 8
         } else if (angleInDegrees < 250 && angleInDegrees >= 240) {
-            leftAngle -= 40
+            leftAngle += 5
             rightAngle -= 7
         } else if (angleInDegrees < 240 && angleInDegrees >= 230) {
-            leftAngle -= 50
+            leftAngle += 5
             rightAngle -= 6
         } else if (angleInDegrees < 230 && angleInDegrees >= 220) {
-            leftAngle -= 60
+            leftAngle += 0
             rightAngle -= 5
         } else if (angleInDegrees < 220 && angleInDegrees >= 210) {
-            leftAngle -= 70
+            leftAngle += 0
             rightAngle -= 5
         } else if (angleInDegrees < 210 && angleInDegrees >= 200) {
-            leftAngle -= 80
+            leftAngle += 0
             rightAngle -= 5
         } else if (angleInDegrees < 200 && angleInDegrees >= 190) {
-            leftAngle -= 90
+            leftAngle += 0
             rightAngle -= 5
         } else if (angleInDegrees < 190 && angleInDegrees >= 180) {
-            leftAngle -= 100
+            leftAngle += 0
             rightAngle += 1
         }
     }
