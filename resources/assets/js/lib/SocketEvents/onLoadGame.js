fd44cb2bd989241996db773c6f50644355334b76
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-11-03 08:01:50 -0700

    Update the url on load game

--- resources/assets/js/lib/SocketEvents/onLoadGame.js
+++ resources/assets/js/lib/SocketEvents/onLoadGame.js
@@ -6,5 +6,9 @@ export default function onLoadGame(data) {
     const store = this.game.store
     store.dispatch(actions.room.setRoom(data.room))
     store.dispatch(actions.game.setChatMessages(data.room.messages.slice(-5)))
+
+    const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?roomId=' + data.room.id
+    window.history.pushState({ path: newurl }, '', newurl)
+
     this.game.state.start('Preloader', false)
 }
