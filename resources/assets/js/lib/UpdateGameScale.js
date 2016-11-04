aa7ee454bff49bfaba888e2f5680a514abbcde61
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-11-04 07:11:05 -0700

    Just account for rescaling the tiles
    So they're in view

--- resources/assets/js/lib/UpdateGameScale.js
+++ resources/assets/js/lib/UpdateGameScale.js
@@ -1,11 +1,12 @@
 // import $ from 'jquery'
 
 export default function() {
-    const qualities = [1000, 600, 300]
-    const scaleFactor = qualities[0]
+    // const qualities = [1000, 600, 300]
+    // const scaleFactor = qualities[0]
     const innerWidth = window.innerWidth
     const innerHeight = window.innerHeight
     const gameRatio = innerWidth / innerHeight
+    const scaleFactor = innerWidth
 
     const widthScaleFactor = Math.ceil(scaleFactor * gameRatio)
     const heightScaleFactor = scaleFactor
@@ -15,6 +16,6 @@ export default function() {
     //     "transform-origin": "top right",
     // })
 
-    this.game.scale.setGameSize(widthScaleFactor, heightScaleFactor)
+    // this.game.scale.setGameSize(widthScaleFactor, heightScaleFactor)
     RS.tiles && RS.tiles.resize(widthScaleFactor, heightScaleFactor)
 }
