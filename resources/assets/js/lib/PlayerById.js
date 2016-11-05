d2e29a07f26984317a596fb1a242bbd7164916d5
daniphilia <dani.philia@gmail.com>
2016-11-04 23:27:10 -0400

    Add union types indicated by

--- resources/assets/js/lib/PlayerById.js
+++ resources/assets/js/lib/PlayerById.js
@@ -1,6 +1,7 @@
 // @flow
 
-export default function PlayerById(id: string) {
+// returns either an object a boolean or nothing
+export default function PlayerById(id: string): Object | boolean | void  {
     if (! RS.enemies) return
 
     for (let i = 0; i < RS.enemies.children.length; i++) {
