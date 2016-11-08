7f0cd3aa934f4ddc8dd21b0f63431e0bfc2a5b11
Michael J. Calkins <michaeljamescalkins@gmail.com>
2016-11-08 15:43:30 -0800

    Optimize player deaths

--- resources/assets/js/lib/PlayPlayerDeathAnimation.js
+++ resources/assets/js/lib/PlayPlayerDeathAnimation.js
@@ -6,7 +6,6 @@ export default function(data: {
 }) {
     const playerDeath = RS.playerDeaths.getFirstExists(false)
     playerDeath.reset(data.x, data.y)
-    playerDeath.scale.setTo(.31)
     playerDeath.anchor.setTo(.5)
     playerDeath.animations.play('death', 17)
     playerDeath.animations.currentAnim.onComplete.add(() => {
