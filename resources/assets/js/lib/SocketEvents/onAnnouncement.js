c10fb57b087daa608c19276f7900a36caa44e00a
Andżelo Mazurek <andre2go@gmail.com>
2016-12-13 00:34:22 +0100

    Add onAnnouncement event handler

---
+++ resources/assets/js/lib/SocketEvents/onAnnouncement.js
@@ -0,0 +1,9 @@
+// @flow
+
+import actions from 'actions'
+
+export default function onAnnouncement(
+  announcement: string
+) {
+    this.game.store.dispatch(actions.room.addAnnouncement(announcement))
+}
