b4e20bd2d83242b6fbc9ea3bfac74fe3550ab8f7
Andżelo Mazurek <andre2go@gmail.com>
2016-12-13 00:35:48 +0100

    Add password protected admin route

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,16 @@
 'use strict'
 
 var express = require('express')
+var basicAuth = require('basicauth-middleware')
 var router = express.Router()
 var MainController = require('./controllers/MainController')
+
+var auth = basicAuth('admin', '!rs(;;)')
 
 router.get('/', MainController.home)
 router.get('/game', MainController.game)
 router.get('/credits', MainController.credits)
+router.get('/admin', auth, MainController.admin)
+router.post('/admin', auth, MainController.adminAnnouncement)
 
 module.exports = router
