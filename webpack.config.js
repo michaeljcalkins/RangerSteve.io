dba215076078ca83c6ae3d6edb39b899e7fbbb2b
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-02 14:33:57 -0800

    Reorganize webpack file for readability

--- webpack.config.js
+++ webpack.config.js
@@ -1,147 +1,110 @@
 'use strict'
 
-const
-    path = require('path'),
-    webpack = require('webpack'),
+const path = require('path')
+const webpack = require('webpack')
 
-    // Development Plugins
-    BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
+// Plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeJsPlugin = require('optimize-js-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const JavaScriptObfuscator = require('webpack-obfuscator')
 
-    // Production Plugins
-    JavaScriptObfuscator = require('webpack-obfuscator'),
+// Config
+const PROD = process.argv[2] === '-p' || process.env.NODE_ENV === 'production'
+const ASSET_PATH = 'resources/assets'
+const APP_FILE_PATH = './resources/assets/js/app.js'
+const PUBLIC_PATH = 'public'
 
-    // Shared Plugins
-    ExtractTextPlugin = require("extract-text-webpack-plugin"),
-    OptimizeJsPlugin = require("optimize-js-plugin"),
-
-    // Analysis Plugins
-    // BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
-
-    // Config
-    isProduction = process.argv[2] === '-p',
-    SRC = 'resources/assets',
-    APP = `${SRC}/js/app.js`,
-    DIST = 'public';
-
-let sharedConfig = {
-    devtool: "eval-source-map",
-    entry: {
-        index: path.join(__dirname, APP),
-    },
-    output: {
-        path: path.join(__dirname, DIST),
-        filename: 'js/app.js',
-        publicPath: '/',
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'css/app.css',
-            allChunks: true,
-            disable: false,
-        }),
-    ],
-    module: {
-        loaders: [
-            {
-                test:/\.(js|jsx)$/,
-                include: [
-                    path.join(__dirname, `${SRC}/js`),
-                    path.resolve('node_modules/preact-compat/src'),
-                ],
-                loader: 'babel',
-
-            },
-            {
-                test: /\.scss$/, include: [
-                    path.join(__dirname, `${SRC}/sass/app.scss`), // important for performance!
-                ], exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract({
-                    fallbackLoader: 'style',
-                    loader: 'css!sass',
-                }),
-            },
-        ],
-    },
-    resolve: {
-        alias: {
-            lib: path.resolve('./lib'),
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat',
-            'actions': path.resolve('./resources/assets/js/actions/'),
+/**
+ * These change based on the environment. By default
+ * we assume the current environment
+ * is "Development".
+ */
+module.exports = {
+  devtool: PROD ? 'source-map' : 'eval-source-map',
+  entry: APP_FILE_PATH,
+  output: {
+    path: PUBLIC_PATH,
+    filename: 'js/app.js',
+    publicPath: '/',
+  },
+  plugins: PROD
+    ? [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': `'production'`,
         },
-        extensions: ['.scss', '.webpack.js', '.web.js', '.js'],
+      }),
+      new ExtractTextPlugin({
+        filename: 'css/app.css',
+        allChunks: true,
+        disable: false,
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        mangle: true,
+        minimize: true,
+        compressor: {
+          warnings: false,
+          screw_ie8: true,
+        },
+      }),
+      new OptimizeJsPlugin({
+        sourceMap: false,
+      }),
+      new JavaScriptObfuscator({
+        selfDefending: true,
+      }),
+    ]
+    : [
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        proxy: {
+          target: 'http://localhost:3000',
+          ws: true,
+        },
+      }),
+      new ExtractTextPlugin({
+        filename: 'css/app.css',
+        allChunks: true,
+        disable: false,
+      }),
+      new OptimizeJsPlugin({
+        sourceMap: false,
+      }),
+    ]
+  ,
+  module: {
+    loaders: [
+      {
+        test:/\.(js|jsx)$/,
+        include: [
+          path.join(__dirname, `${ASSET_PATH}/js`),
+          path.resolve('node_modules/preact-compat/src'),
+        ],
+        loader: 'babel',
+      },
+      {
+        test: /\.scss$/, include: [
+          path.join(__dirname, `${ASSET_PATH}/sass/app.scss`), // important for performance!
+        ], exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style',
+          loader: 'css!sass',
+        }),
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      lib: path.resolve('./lib'),
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat',
+      'actions': path.resolve('./resources/assets/js/actions/'),
     },
+    extensions: ['.scss', '.webpack.js', '.web.js', '.js'],
+  },
 }
-
-let config = Object.assign({}, sharedConfig)
-
-if (! isProduction) {
-    /**
-     * Development
-     */
-    config.resolve.modules = [
-        path.resolve(__dirname, "resources/assets"),
-        "node_modules",
-    ]
-
-    config.plugins = [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            proxy: {
-                target: "http://localhost:3000",
-                ws: true,
-            },
-        }),
-        new ExtractTextPlugin({
-            filename: 'css/app.css',
-            allChunks: true,
-            disable: false,
-        }),
-        new webpack.DllReferencePlugin({
-            context: path.join(__dirname, APP),
-            manifest: require("./dll/vendor-manifest.json"),
-        }),
-        // uncomment to analyze webpack bundle size
-        // new BundleAnalyzerPlugin(),
-        new OptimizeJsPlugin({
-            sourceMap: false,
-        }),
-    ]
-} else {
-    /**
-     * Production
-     */
-    config.devtool = "source-map"
-    config.plugins = [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': `"production"`,
-            },
-        }),
-        new ExtractTextPlugin({
-            filename: 'css/app.css',
-            allChunks: true,
-            disable: false,
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            mangle: true,
-            minimize: true,
-            compressor: {
-                warnings: false,
-                screw_ie8: true,
-            },
-        }),
-        new OptimizeJsPlugin({
-            sourceMap: false,
-        }),
-        new JavaScriptObfuscator({
-            selfDefending: true,
-        }),
-    ]
-}
-
-module.exports = config
