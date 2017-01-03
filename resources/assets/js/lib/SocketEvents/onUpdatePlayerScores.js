e6404c60be0f899b9fc603bb1e1c6984bc2f8efa
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-02 23:15:00 -0800

    Add update player score logic

---
+++ resources/assets/js/lib/SocketEvents/onUpdatePlayerScores.js
@@ -0,0 +1,23 @@
+import actions from 'actions'
+
+export default function (data) {
+  const store = this.game.store
+
+  if (! data.players) return
+  const room = store.getState().room
+
+  Object.keys(data.players).forEach(playerId => {
+      if (! data.players[playerId]) return
+
+      room.players[playerId] = {
+          ...room.players[playerId],
+          ...data.players[playerId],
+      }
+  })
+
+  room.blueTeamScore = data.blueTeamScore
+  room.redTeamScore = data.redTeamScore
+
+
+  store.dispatch(actions.room.setRoom(room))
+}
