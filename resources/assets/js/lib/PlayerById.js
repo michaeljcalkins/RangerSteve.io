989ca3211e7350919cf7532723e74077a22f39fb
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-02 22:31:37 -0800

    Format file

--- resources/assets/js/lib/PlayerById.js
+++ resources/assets/js/lib/PlayerById.js
@@ -1,12 +1,12 @@
 import get from 'lodash/get'
 
 // Returns either an object a boolean or false
-export default function PlayerById(id) {
-    if (! RS.enemies) return
+export default function PlayerById (id) {
+  if (!RS.enemies) return
 
-    for (let i = 0; i < RS.enemies.children.length; i++) {
-        if (get(RS, `enemies.children[${i}].data.id`) === id) {
-            return RS.enemies.children[i]
-        }
+  for (let i = 0; i < RS.enemies.children.length; i++) {
+    if (get(RS, `enemies.children[${i}].data.id`) === id) {
+      return RS.enemies.children[i]
     }
+  }
 }
--- resources/assets/js/lib/removePlayersThatLeft.js
+++ resources/assets/js/lib/removePlayersThatLeft.js
@@ -3,24 +3,24 @@ import get from 'lodash/get'
 import actions from 'actions'
 
 export default function removePlayersThatLeft(data) {
-    if (! RS.enemies) return
+  if (!RS.enemies) return
 
-    const store = this.game.store
-    const room = this.game.store.getState().room
+  const store = this.game.store
+  const room = this.game.store.getState().room
 
-    RS.enemies.forEach((player, index) => {
-        const playerId = get(player, 'data.id', false)
+  RS.enemies.forEach((player, index) => {
+    const playerId = get(player, 'data.id', false)
 
-        // Does this enemy still exist in the data sent from the server
-        const enemy = data.players[playerId]
+    // Does this enemy still exist in the data sent from the server
+    const enemy = data.players[playerId]
 
-        // Enemy not found so remove them from game
-        if (! enemy && playerId) {
-            RS.enemies.removeChildAt(index)
-            player.destroy(true)
+    // Enemy not found so remove them from game
+    if (! enemy && playerId) {
+      RS.enemies.removeChildAt(index)
+      player.destroy(true)
 
-            delete room.players[playerId]
-            store.dispatch(actions.room.setRoom(room))
-        }
-    })
+      delete room.players[playerId]
+      store.dispatch(actions.room.setRoom(room))
+    }
+  })
 }
