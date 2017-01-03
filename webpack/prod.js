7daaa870585131387ce8588c2a9d08e28ca58e49
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-02 16:45:46 -0800

    Refactor webpack into env files and remove obfuscation

---
+++ webpack/prod.js
@@ -0,0 +1,46 @@
+const webpackMerge = require('webpack-merge')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const OptimizeJsPlugin = require('optimize-js-plugin')
+// const JavaScriptObfuscator = require('webpack-obfuscator')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const commonConfig = require('./base.js')
+
+module.exports = function (env) {
+  return webpackMerge(commonConfig(), {
+    devtool: 'source-map',
+    plugins: [
+      new LodashModuleReplacementPlugin,
+      new BundleAnalyzerPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': `'production'`,
+        },
+      }),
+      new ExtractTextPlugin({
+        filename: 'css/app.css',
+        allChunks: true,
+        disable: false,
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        mangle: true,
+        minimize: true,
+        compressor: {
+          warnings: false,
+          screw_ie8: true,
+        },
+      }),
+      new OptimizeJsPlugin({
+        sourceMap: false,
+      }),
+      // new JavaScriptObfuscator({
+      //   selfDefending: true,
+      // }),
+    ],
+  })
+}
