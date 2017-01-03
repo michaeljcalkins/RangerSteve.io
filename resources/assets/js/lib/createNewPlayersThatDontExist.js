3fccf7924084b7896a4afbc3841c86d7dace3d41
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-02 22:28:57 -0800

    Format file

--- resources/assets/js/lib/createNewPlayersThatDontExist.js
+++ resources/assets/js/lib/createNewPlayersThatDontExist.js
@@ -1,36 +1,37 @@
 import createRemotePlayer from './createRemotePlayer'
 import updatePlayerColor from './updatePlayerColor'
-import PlayerById from'./PlayerById'
+import PlayerById from './PlayerById'
+import get from 'lodash/get'
 
 const usernameTextStyle = {
-    align: "center",
-    fill: "#fff",
-    font: "10px Arial",
-    stroke: "black",
-    strokeThickness: 2,
+  align: 'center',
+  fill: '#fff',
+  font: '10px Arial',
+  stroke: 'black',
+  strokeThickness: 2,
 }
 
-export default function createNewPlayersThatDontExist(room, playerId, playerData) {
-    const playerCheck = PlayerById.call(this, playerId)
+export default function createNewPlayersThatDontExist (room, playerId, playerData) {
+  const playerCheck = PlayerById.call(this, playerId)
 
-    if (playerCheck) return
+  if (playerCheck) return
 
-    const newcreateRemotePlayer = createRemotePlayer.call(this, playerId, playerData)
-    const enemyPlayerName = playerData.nickname
-        ? playerData.nickname
-        : 'Unnamed Ranger'
+  const newCreateRemotePlayer = createRemotePlayer.call(this, playerId, playerData)
+  const enemyPlayerName = playerData.nickname
+    ? playerData.nickname
+    : 'Unnamed Ranger'
 
-    const text = this.game.add.text(0, -50, enemyPlayerName, usernameTextStyle)
-    newcreateRemotePlayer.addChild(text)
-    text.x = (text.width / 2) * -1
-    text.smoothed = true
+  const text = this.game.add.text(0, -50, enemyPlayerName, usernameTextStyle)
+  newCreateRemotePlayer.addChild(text)
+  text.x = (text.width / 2) * -1
+  text.smoothed = true
 
-    if (playerData.health <= 0) {
-        newcreateRemotePlayer.visible = false
-    }
+  if (playerData.health <= 0) {
+    newCreateRemotePlayer.visible = false
+  }
 
-    RS.enemies.add(newcreateRemotePlayer)
-    const player = PlayerById.call(this, playerId)
-    this.game.world.bringToTop(RS.enemies)
-    if (playerData.team) updatePlayerColor(player, playerData.team)
+  RS.enemies.add(newCreateRemotePlayer)
+  const player = PlayerById.call(this, playerId)
+  this.game.world.bringToTop(RS.enemies)
+  if (playerData.team) updatePlayerColor(player, playerData.team)
 }
