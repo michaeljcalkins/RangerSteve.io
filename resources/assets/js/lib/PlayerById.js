de8922fc6443963f55db08b5dd7a6846124ccff1
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-09 10:11:36 -0800

    Format changes

--- resources/assets/js/lib/PlayerById.js
+++ resources/assets/js/lib/PlayerById.js
@@ -2,7 +2,7 @@ import get from 'lodash/get'
 
 // Returns either an object a boolean or false
 export default function PlayerById (id) {
-  if (!RS.enemies) return
+  if (! RS.enemies) return
 
   for (let i = 0; i < RS.enemies.children.length; i++) {
     if (get(RS, `enemies.children[${i}].data.id`) === id) {
