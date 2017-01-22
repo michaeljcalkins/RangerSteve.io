45ce1eb06c0206b523e0912ef18cabe6e3d1711a
Andżelo <andre2go@gmail.com>
2017-01-22 20:24:43 +0100

    Cancel reloading while switching weapons

--- resources/assets/js/lib/ReloadGunWhenEmpty.js
+++ resources/assets/js/lib/ReloadGunWhenEmpty.js
@@ -6,36 +6,40 @@ export default function (currentWeaponId) {
   const isPrimarySelected = store.getState().player.currentWeapon === 'primaryWeapon'
   const currentWeapon = GameConsts.WEAPONS[currentWeaponId]
 
-    // Get ammo remaining in current gun
+  // Get ammo remaining in current gun
   const currentAmmoRemaining = isPrimarySelected
-        ? store.getState().player.primaryAmmoRemaining
-        : store.getState().player.secondaryAmmoRemaining
+    ? store.getState().player.primaryAmmoRemaining
+    : store.getState().player.secondaryAmmoRemaining
 
   if (
-        currentAmmoRemaining <= 0 &&
-        (
-            (isPrimarySelected && !store.getState().player.isPrimaryReloading) ||
-            (!isPrimarySelected && !store.getState().player.isSecondaryReloading)
-        )
-    ) {
-        // If empty set current gun to reloading
-    if (isPrimarySelected) {
-      store.dispatch(actions.player.setPrimaryIsReloading(true))
-    } else {
-      store.dispatch(actions.player.setSecondaryIsReloading(true))
+    currentAmmoRemaining > 0 ||
+    store.getState().player.isPrimaryReloading ||
+    store.getState().player.isSecondaryReloading
+  ) return
+
+  // If empty set current gun to reloading
+  if (isPrimarySelected) {
+    store.dispatch(actions.player.setPrimaryIsReloading(true))
+  } else {
+    store.dispatch(actions.player.setSecondaryIsReloading(true))
+  }
+
+  // Get reload time in seconds
+  setTimeout(() => {
+    if (store.getState().player.hasCaneledReloading) {
+      store.dispatch(actions.player.setHasCanceledReloading(false))
+      return
     }
 
-        // Get reload time in seconds
-    setTimeout(() => {
-      if (isPrimarySelected) {
-        store.dispatch(actions.player.setPrimaryIsReloading(false))
-        store.dispatch(actions.player.setPrimaryAmmoRemaining(currentWeapon.ammo))
-        return
-      }
+    if (isPrimarySelected && store.getState().player.isPrimaryReloading) {
+      store.dispatch(actions.player.setPrimaryIsReloading(false))
+      store.dispatch(actions.player.setPrimaryAmmoRemaining(currentWeapon.ammo))
+      return
+    }
 
+    if (store.getState().player.isSecondaryReloading) {
       store.dispatch(actions.player.setSecondaryIsReloading(false))
       store.dispatch(actions.player.setSecondaryAmmoRemaining(currentWeapon.ammo))
-    }, currentWeapon.reloadTime)
-    return
-  }
+    }
+  }, currentWeapon.reloadTime)
 }
