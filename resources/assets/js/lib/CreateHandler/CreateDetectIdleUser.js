f709b3ee9de52473cf00a652101a0e19f0ebb6a8
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-01-28 12:56:46 -0800

    Add web worker for background idle detection

--- resources/assets/js/lib/CreateHandler/CreateDetectIdleUser.js
+++ resources/assets/js/lib/CreateHandler/CreateDetectIdleUser.js
@@ -1,12 +1,22 @@
-import GameConsts from 'lib/GameConsts'
-import debounce from 'lodash/debounce'
+// Creates a Web Worker
+var worker = new window.Worker('/js/idlePlayerDetectionWorker.js')
 
-const playerIsIdle = debounce(() => {
+// Triggered by postMessage in the Web Worker
+worker.onmessage = function (evt) {
+  // When a message is sent back that means the player has been idle for X amount of seconds
   document.location.href = '/'
-}, GameConsts.MAX_IDLE_SECONDS * 1000)
+}
+
+// If the Web Worker throws an error
+worker.onerror = function (evt) {
+  console.error(evt.data)
+}
 
 export default function () {
-  window.onload = playerIsIdle
-  document.onmousemove = playerIsIdle
-  document.onkeypress = playerIsIdle
+  worker.postMessage(null)
+
+  // These postMessage's work like a ping to run the debouce function that is run in the background
+  window.onload = () => { worker.postMessage(null) }
+  document.onmousemove = () => { worker.postMessage(null) }
+  document.onkeypress = () => { worker.postMessage(null) }
 }
