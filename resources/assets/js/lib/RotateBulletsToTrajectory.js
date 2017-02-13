b962dddec8f27e03c5f997639c5925590e6b1cd9
Andżelo <andre2go@gmail.com>
2017-02-14 00:25:22 +0100

    Enable bullet rotation again

--- resources/assets/js/lib/RotateBulletsToTrajectory.js
+++ resources/assets/js/lib/RotateBulletsToTrajectory.js
@@ -2,11 +2,11 @@
  * Rotate bullets according to trajectory
  */
 export default function () {
-  // window.RS.bullets.forEachAlive(bullet => {
-  //   bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x)
-  // })
+  window.RS.bullets.forEachAlive(bullet => {
+    bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x)
+  })
 
-  // window.RS.enemyBullets.forEachAlive(bullet => {
-  //   bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x)
-  // })
+  window.RS.enemyBullets.forEachAlive(bullet => {
+    bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x)
+  })
 }
