5c6b80ae788339ca216ac0c9587870c1f44017ba
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-02-16 08:04:24 -0800

    Remove no polygon maps and add collisions file

--- resources/assets/js/lib/Maps/index.js
+++ resources/assets/js/lib/Maps/index.js
@@ -1,19 +1,13 @@
 import * as DarkForest from './DarkForest'
-import * as DeathCycle from './DeathCycle'
 import * as EvilUnderground from './EvilUnderground'
 import * as HighRuleJungle from './HighRuleJungle'
-import * as PunkCity from './PunkCity'
 import * as PunkFallout from './PunkFallout'
-import * as PunkLoop from './PunkLoop'
 import * as Eclipse from './Eclipse'
 
 export default {
   DarkForest,
-  DeathCycle,
   EvilUnderground,
   HighRuleJungle,
-  PunkCity,
   PunkFallout,
-  PunkLoop,
   Eclipse
 }
