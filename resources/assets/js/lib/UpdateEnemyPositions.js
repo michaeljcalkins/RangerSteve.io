22541d1955103e530b33fd166aeea8f5741cfe03
Andżelo <andre2go@gmail.com>
2017-02-23 13:10:48 +0100

    Use buffer for enemy player positions to improve entity interpolation

--- resources/assets/js/lib/UpdateEnemyPositions.js
+++ resources/assets/js/lib/UpdateEnemyPositions.js
@@ -1,5 +1,7 @@
 import GameConsts from 'lib/GameConsts'
 
+const interpolationBackTimeMs = 100
+
 export default function () {
   const state = this.game.store.getState()
 
@@ -9,19 +11,41 @@ export default function () {
     !window.RS.enemies
   ) return
 
+  const currentTime = Date.now()
+  // const currentTime = state.room.currentTime
+  const interpolationTime = currentTime - interpolationBackTimeMs
+
   window.RS.enemies.forEach((enemy) => {
-    const targetPosition = enemy.data.targetPosition
+    const { positionBuffer } = enemy.data
 
-    if (
-      !targetPosition ||
-      targetPosition.millisRemaining < 0
-    ) return
+    if (!positionBuffer || !positionBuffer.length) {
+      return
+    }
 
-    const weight = 1 - targetPosition.millisRemaining / GameConsts.TICK_INTERVAL_MS // between 0.0 and 1.0
+    if (positionBuffer[0].time > interpolationTime) {
+      let i = 0
+      const positionBufferLength = positionBuffer.length
+      for (; i < positionBufferLength; i++) {
+        // find the closest state that matches network time, or use oldest state
+        if (positionBuffer[i].time <= interpolationTime || i === positionBufferLength - 1) {
+          // the state closest to network time
+          const closestState = positionBuffer[i]
+          // the state one slot newer
+          const nextClosestState = positionBuffer[Math.max(i - 1, 0)]
+          // use time between closestState and nextClosestState to interpolate
+          const length = nextClosestState.time - closestState.time
 
-    enemy.x = this.game.math.linear(enemy.x || 0, targetPosition.x, weight)
-    enemy.y = this.game.math.linear(enemy.y || 0, targetPosition.y, weight)
+          let t = 0
+          if (length > 0.0001) {
+            t = Math.max(0, (interpolationTime - closestState.time) / length)
+          }
 
-    targetPosition.millisRemaining = Math.max(targetPosition.millisRemaining - this.game.time.elapsed, 0)
+          enemy.x = this.game.math.linear(closestState.x, nextClosestState.x, t)
+          enemy.y = this.game.math.linear(closestState.y, nextClosestState.y, t)
+
+          break
+        }
+      }
+    }
   })
 }
