9c9542e06acd6fb686f45773435893da9bded126
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-03-06 06:57:19 -0800

    Remove iframe check and electron

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,10 +4,7 @@ import { createStore } from 'redux'
 import rootReducer from './reducers'
 import ui from './ui'
 import game from './game'
-import checkForIframeAndRedirect from './lib/checkForIframeAndRedirect'
 
-window.IS_ELECTRON = window && window.process && window.process.type
-checkForIframeAndRedirect()
 const store = createStore(rootReducer)
 
 ui(store)
