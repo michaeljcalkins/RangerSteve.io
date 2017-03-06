2a61c317b565634a5cace9380b29569f42e79989
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-03-05 17:34:50 -0800

    add iogames.spcae to safe urls

--- resources/assets/js/lib/checkForIframeAndRedirect.js
+++ resources/assets/js/lib/checkForIframeAndRedirect.js
@@ -2,7 +2,10 @@
  * Prevent our site from being iframed
  */
 export default function () {
-  if (window.self !== window.top) {
-    window.top.location = window.location // redirect the window to our website
-  }
+  if (
+    window.self === window.top ||
+    window.top.location.href.indexOf('iogames.space') > -1
+  ) return
+
+  window.top.location = window.location // redirect the window to our website
 }
