dbba2a5b8eecfd2828bb8e08ad60b26b70f99f63
Andżelo <andre2go@gmail.com>
2017-03-08 22:23:07 +0100

    Add a way to select the Entity Interpolation method on the client (Disabled, Basic, Advanced)

--- resources/assets/js/lib/UpdateEnemyPositions.js
+++ resources/assets/js/lib/UpdateEnemyPositions.js
@@ -1,5 +1,7 @@
 import storage from 'store'
 
+import GameConsts from 'lib/GameConsts'
+
 const interpolationBackTimeMs = storage.get('interpolationMs', 100)
 
 export default function () {
@@ -11,40 +13,58 @@ export default function () {
     !window.RS.enemies
   ) return
 
-  const currentTime = Date.now() + window.socket.offset
-  const interpolationTime = currentTime - interpolationBackTimeMs
+  if (state.game.entityInterpolationType === GameConsts.ENTITY_INTERPOLATION_TYPE.BASIC) {
+    window.RS.enemies.forEach((enemy) => {
+      const targetPosition = enemy.data.targetPosition
 
-  window.RS.enemies.forEach((enemy) => {
-    if (!enemy.data.positionBuffer || !enemy.data.positionBuffer.length) {
-      return
-    }
+      if (
+        !targetPosition ||
+        targetPosition.millisRemaining < 0
+      ) return
 
-    const positionBuffer = JSON.parse(JSON.stringify(enemy.data.positionBuffer))
+      const weight = 1 - targetPosition.millisRemaining / GameConsts.TICK_RATE // between 0.0 and 1.0
 
-    if (positionBuffer[0].time > interpolationTime) {
-      let i = 0
-      const positionBufferLength = positionBuffer.length
-      for (; i < positionBufferLength; i++) {
-        // find the closest state that matches network time, or use oldest state
-        if (positionBuffer[i].time <= interpolationTime || i === positionBufferLength - 1) {
-          // the state closest to network time
-          const closestState = positionBuffer[i]
-          // the state one slot newer
-          const nextClosestState = positionBuffer[Math.max(i - 1, 0)]
-          // use time between closestState and nextClosestState to interpolate
-          const length = nextClosestState.time - closestState.time
+      enemy.x = this.game.math.linear(enemy.x || 0, targetPosition.x, weight)
+      enemy.y = this.game.math.linear(enemy.y || 0, targetPosition.y, weight)
 
-          let t = 0
-          if (length > 0.0001) {
-            t = Math.max((interpolationTime - closestState.time) / length, 0)
+      targetPosition.millisRemaining = Math.max(targetPosition.millisRemaining - this.game.time.elapsed, 0)
+    })
+  } else {
+    const currentTime = Date.now() + window.socket.offset
+    const interpolationTime = currentTime - interpolationBackTimeMs
+
+    window.RS.enemies.forEach((enemy) => {
+      if (!enemy.data.positionBuffer || !enemy.data.positionBuffer.length) {
+        return
+      }
+
+      const positionBuffer = JSON.parse(JSON.stringify(enemy.data.positionBuffer))
+
+      if (positionBuffer[0].time > interpolationTime) {
+        let i = 0
+        const positionBufferLength = positionBuffer.length
+        for (; i < positionBufferLength; i++) {
+          // find the closest state that matches network time, or use oldest state
+          if (positionBuffer[i].time <= interpolationTime || i === positionBufferLength - 1) {
+            // the state closest to network time
+            const closestState = positionBuffer[i]
+            // the state one slot newer
+            const nextClosestState = positionBuffer[Math.max(i - 1, 0)]
+            // use time between closestState and nextClosestState to interpolate
+            const length = nextClosestState.time - closestState.time
+
+            let t = 0
+            if (length > 0.0001) {
+              t = Math.max((interpolationTime - closestState.time) / length, 0)
+            }
+
+            enemy.x = this.game.math.linear(closestState.x, nextClosestState.x, t)
+            enemy.y = this.game.math.linear(closestState.y, nextClosestState.y, t)
+
+            break
           }
-
-          enemy.x = this.game.math.linear(closestState.x, nextClosestState.x, t)
-          enemy.y = this.game.math.linear(closestState.y, nextClosestState.y, t)
-
-          break
         }
       }
-    }
-  })
+    })
+  }
 }
