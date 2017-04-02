56d489ad9d6eff654ca07b6bd8080b0fd9baebe3
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-04-02 16:04:07 -0700

    Extract calculation above

--- resources/assets/js/ui/components/SettingsModal/WeaponStats.js
+++ resources/assets/js/ui/components/SettingsModal/WeaponStats.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function ({ weapon }) {
+  const fireRate = (1000 / weapon.fireRate).toFixed(1)
+
   return (
     <div className='option-stats'>
       <div className='row'>
@@ -10,9 +12,7 @@ export default function ({ weapon }) {
         </div>
         <div className='col-xs-4'>
           <h5>Fire Rate</h5>
-          <h2>
-            { (1000 / weapon.fireRate).toFixed(1) }
-          </h2>
+          <h2>{ fireRate }</h2>
         </div>
         <div className='col-xs-4'>
           <h5>Capacity</h5>
